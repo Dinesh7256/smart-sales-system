98b061f05236f1a1957802dc54b1613bd9639510
Dinesh kumar <dineshkumar61175@gmail.com>
2025-08-06 17:55:32 +0530

    password hashing and login api created

--- backend/service/user-service.js
+++ backend/service/user-service.js
@@ -23,25 +23,25 @@ class UserService {
         }
     }
 
-    // async signin(data) {
-    //     try {
-    //         const user = await this.getUserByEmail(data.email);
-    //         if(!user) {
-    //             throw {
-    //                 message: 'no user found'
-    //             };
-    //         }
-    //         if(!user.comparePassword(data.password)) {
-    //             throw {
-    //                 message: 'incorrect password',
-    //             };
-    //         }
-    //         const token = user.genJWT();
-    //         return token;
-    //     } catch(error) {
-    //         throw error;
-    //     }
-    // }
+    async signin(data) {
+        try {
+            const user = await this.getUserByEmail(data.email);
+            if(!user) {
+                throw {
+                    message: 'no user found'
+                };
+            }
+            if(!user.comparePassword(data.password)) {
+                throw {
+                    message: 'incorrect password',
+                };
+            }
+            const token = user.genJWT();
+            return token;
+        } catch(error) {
+            throw error;
+        }
+    }
 }
 
 export default UserService;
