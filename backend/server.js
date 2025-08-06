fd1a0128e802e06aba682b73b6bb4e01fe265941
Dinesh kumar <dineshkumar61175@gmail.com>
2025-08-06 16:10:02 +0530

    signup api  localhost:5001/api/v1/signup checked

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const { connect } = require('./config/database.js');
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import { connect } from './config/database.js';
+import apiroutes from './routes/index.js';
+// import passport, { use } from 'passport';
+// import User from './models/user.js';
 
 dotenv.config();
 
@@ -11,10 +14,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+// app.use(passport.initialize());
+
+app.use('/api', apiroutes);
+
 app.listen(PORT, async()=>{
     console.log(`Server Started : ${PORT}`);
     await connect();
     console.log('Database connection established');
+
+    // const user = new User({
+    //     businessName: 'shobha kirana store',
+    //     email: 'shobhakiranastore@gmail.com',
+    //     password: 'password123'
+    // });
+
+    // await user.save();
+    // console.log('User created:', user);
 })
 
 
